5bdfa8e682984fecd79c598b42359425b37160dc
Jeffrey Ganulin <jganulin6@gmail.com>
2023-03-16 21:38:16 -0400

    * Added React scaffolding with client-side routing * Replaced template pages with custom placeholders * Added basic logo and favicons * Created .gitignores to ignore IDE-specific settings, caches, etc. * Created resuable components LabeledInput, CustomLink, Header

---
+++ frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+// Attach React functionality to the root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// Render the App component inside the root element, representing the entire app
+// We can wrap <App  /> with providers that provide context/features to all child components
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { toast } from 'react-hot-toast';
+
+import CustomLink from '../components/CustomLink';
+import LabeledInput from '../components/LabeledInput';
+
+
+/** <Login />
+ * @description Renders the login page
+ * @params {Object} props - The props passed to the component
+ * @returns {JSX.Element} <Login />
+ */
+export default function Login(props) {
+    const [formState, setFormState] = useState({});
+
+    // When the submit button is clicked, this function is called
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        toast.success('You clicked the login button');
+    };
+
+    // Whenever a form input changes, this function is called
+    const handleInputChange = (e) => {
+        // Get the ID and value of the input that changed
+        const { id, value } = e.target;
+        // Update the form state with the new value for the changed input
+        setFormState((prevState) => ({
+            ...prevState,
+            [id]: value,
+        }));
+    };
+
+    return (
+        <>
+            <Helmet> {/* This allows us to change the page title, meta tags, etc. */}
+                <title>Intrepid - Login</title>
+            </Helmet>
+            <h1>Login</h1>
+            <p>Don't have an account yet? <CustomLink to="/register">Register today.</CustomLink></p>
+            <form onSubmit={handleSubmit}>
+                <LabeledInput label="Email" id="email" name="email" type="email" value={formState.email} onChange={handleInputChange} required />
+                <LabeledInput label="Password" id="password" name="password" type="password" value={formState.password} onChange={handleInputChange} required />
+                <button type="submit">Login</button>
+            </form>
+            
+            <div style={{ maxWidth: '300px', margin: '0 auto' }}>
+                <h2>Debugging</h2>
+                <p>Form state:</p>
+                <pre>{JSON.stringify(formState, null, 2)}</pre>
+            </div>
+        </>
+    )
+}
