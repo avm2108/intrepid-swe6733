790fe098dbe1b1b6a579920d8564426a8a5ee6e7
Alexis McNeill <amcneill@maxmedia.com>
2023-05-02 21:08:17 -0400

    Editable info in user profile, needs some adjusting and needs API connection for saving

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -41,6 +41,8 @@ export default function UserProfile() {
         }
     });
 
+    const [ editedName, setEditedName ] = useState(user?.name);
+    const [ editedBio, setEditedBio ] = useState(profile?.bio);
     const [agePref, setAgePref] = useState('');
     const [maxDist, setMaxDist] = useState('50');
 
@@ -50,11 +52,23 @@ export default function UserProfile() {
         setMaxDist(obj);
     }
 
+    const handleNameChange = (value) => {
+        setEditedName(value);
+    };
+
+    const handleBioChange = (value) => {
+        setEditedBio(value);
+    }
+
     const beginIGLinking = (e) => {
         e.preventDefault();
         // Navigate to Instagram login page
         window.location.href = `/api/auth/instagram`;
-    }   
+    }
+
+    /* const handleSave = () => {
+        // TODO: API call to see 
+    } */
 
     const handleDelete = async (e) => {
         e.preventDefault();
@@ -112,9 +126,14 @@ export default function UserProfile() {
 
                 <div className={styles.editableInfo}>
                     <h3>Display Name</h3>
-                    <div className={styles.userDetails}>{user?.name}</div>
+                    <div className={styles.userDetails}>
+                        <input type="text" value={editedName} onChange={(e) => handleNameChange(e.target.value)}/>
+                        {/* {user?.name} */}
+                    </div>
                     <h3>Email Address</h3>
-                    <div className={styles.userDetails}>{user?.email}</div>
+                    <div className={styles.userDetails}>
+                        {user?.email}
+                        </div>
                     <h3>Location</h3>
                     <div className={styles.userDetails}>{getStateName(profile?.location?.state)}, USA</div>
 {/*                     <h3>Phone Number</h3>
@@ -122,7 +141,11 @@ export default function UserProfile() {
                     <h3>Media Shared</h3> */}
                     {/* Needs image rendering I think for user images on their profile, this section could also be removed */}
                     <h3>Bio / About Me</h3>
-                    <div className={styles.userDetails}>{profile?.bio}</div>
+                    
+                    <div className={styles.userDetails}>
+                    <input type="text" value={editedBio} onChange={(e) => handleBioChange(e.target.value)}/>
+                        {/* {profile?.bio} */}
+                    </div>
                     <h3>Interests</h3>
                     <div className={styles.userDetails}>
                         {profile?.interests?.map?.((interest, idx) => {
